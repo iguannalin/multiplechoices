8697075714d597e646624fbb72f0848621ab3ed3
seasons986 <annalin12345@gmail.com>
2023-06-02 22:48:02 +0800

    finished

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-window.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+  const sheet = document.getElementById("sheet");
+  let buttons = [];
+  // make the buttons fit the page as best as possible
+  let width = Math.floor((document.body.offsetWidth - 20) / 21);
+  let height = Math.floor((window.innerHeight - 150) / 17);
+  let total = width * height;
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      // every time a button is clicked, choose between:
+      // turning on a random button, or
+      // hiding a lot of other buttons
+      radio.onclick = () => {
+        let r = Math.floor(Math.random() * 10);
+        switch (r % 2) {
+          case 0:
+            buttons[getRandomInt(0, total)].checked = "true";
+            break;
+          default:
+            for (let n = 0; n < getRandomInt(0, total); n++) {
+              buttons[getRandomInt(0, total)].style.visibility = "hidden";
+            }
+            break;
+        }
+        return false;
+      }
+      buttons.push(radio);
+      sheet.appendChild(radio);
+    }
+  }
+});
+
+// from MDN (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
